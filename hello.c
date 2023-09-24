62633146e4841458fae6956af0e3c852f8be11df
Aurutus <emslhy@hotmail.com>
2023-09-24 09:46:44 +0800

    course: try signal

--- hello.c
+++ hello.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char* arg[]) {
 	printf("hello, world\n");
+	int cnt = 0;
+	while (1) {
+		printf("count=%d\n", cnt++);
+		sleep(1);
+	}
 	return 0;
 }

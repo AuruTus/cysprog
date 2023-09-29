3f2361f79eae013912917637a5632bc523328cb5
Aurutus <emslhy@hotmail.com>
2023-09-29 14:45:50 +0800

    lab: lab1 write

--- lab1/syscall/mini-lib.c
+++ lab1/syscall/mini-lib.c
@@ -15,52 +15,66 @@
 #define CALL2(n) "movq $"#n", %%rax\n"
 #define CALL(n) CALL2(n)
 
-int strlen(char *s){
+int strlen(char* s) {
     int n = 0;
-    while(*s++)
+    while (*s++)
         n++;
     return n;
 }
 
-int printf(char *s, ...){
+int printf(char* s, ...) {
     // Your code here:
     // TODO();
     return 0;
 }
 
-int puts(char *s) {
+int puts(char* s) {
     long n = strlen(s);
     long r;
     asm(CALL(SYS_WRITE)
-       "movq $1, %%rdi\n"
-       "movq %1, %%rsi\n"
-       "movq %2, %%rdx\n"
-       "syscall\n"
-       "movq %%rax, %0\n"
-       : "=r"(r)
-       : "r"(s), "r"(n)
-       : "%rax", "%rdi", "%rsi", "%rdx");
+        "movq $1, %%rdi\n"
+        "movq %1, %%rsi\n"
+        "movq %2, %%rdx\n"
+        "syscall\n"
+        "movq %%rax, %0\n"
+        : "=r"(r)
+        : "r"(s), "r"(n)
+        : "%rax", "%rdi", "%rsi", "%rdx");
+    return (int)r;
+}
+
+int write(int fd, int buf, int size) {
+    long r;
+    asm(CALL(SYS_WRITE)
+        "movq $1, %%rdi\n"
+        "movq %1, %%rsi\n"
+        "movq %2, %%rdx\n"
+        "syscall\n"
+        "movq %%rax, %0\n"
+        : "=r"(r)
+        : "r"(fd), "r"(buf), "r"(size)
+        : "%rax", "%rdi", "%rsi", "%rdx");
     return (int)r;
 }
 
 
-void exit(int status){
+void exit(int status) {
     // Your code here:
     // TODO();
-    
+
 }
 
-int alarm(unsigned int seconds){
+int alarm(unsigned int seconds) {
     // Your code here:
     // TODO();
-    
+
 }
 
 
-int sleep(struct timespec *ts, struct timespec *rem){
+int sleep(struct timespec* ts, struct timespec* rem) {
     // Your code here:
     // TODO();
-    
+
 }
 
 

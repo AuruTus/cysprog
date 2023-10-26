d5c77ff37ba96a845689f0b8262bdfe939ac1f17
Aurutus <emslhy@hotmail.com>
2023-10-26 12:09:27 +0800

    lab: lab3 mini http server
    f

--- lab3/partA_server/web-server.c
+++ lab3/partA_server/web-server.c
@@ -24,10 +24,10 @@ do{\
 #define SERVER_PORT 8080
 #define BUF_SIZE 1024
 
-void serve_client(int client_sock, const char* web_root){
+void serve_client(int client_sock, const char* web_root) {
     char request[BUF_SIZE];
     ssize_t bytes_received = read(client_sock, request, sizeof(request) - 1);
-    if(bytes_received < 0){
+    if (bytes_received < 0) {
         perror("read");
         return;
     }
@@ -38,32 +38,72 @@ void serve_client(int client_sock, const char* web_root){
 
     char method[16], path[128];
     int scanResult = sscanf(request, "%s %s", method, path);
-    if(scanResult != 2){
+    if (scanResult != 2) {
         char response[] = "HTTP/1.1 400 Bad Request\r\nContent-Length: 0\r\n\r\n";
         write(client_sock, response, strlen(response));
         close(client_sock);
-        return;  
+        return;
     }
 
     char fullpath[256];
-    snprintf(fullpath, sizeof(fullpath), "%s%s", web_root, path);  
+    snprintf(fullpath, sizeof(fullpath), "%s%s", web_root, path);
 
     // Handle GET requests for serving files.
-    if(strcasecmp(method, "GET") == 0){
+    if (strcasecmp(method, "GET") == 0) {
 
         // Exercise 5.
         // Add your code here:
-        TODO();
+        // TODO();
+        int fd = open(fullpath, O_RDONLY);
+        if (fd == -1) {
+            char response[] =
+                "HTTP/1.1 404 Not Found"
+                "\r\nContent-Type: text/plain"
+                "\r\nContent-Length: 13"
+                "\r\n\r\n404 Not Found";
+            write(client_sock, response, strlen(response));
+        } else {
+            char header[] =
+                "HTTP/1.1 200 OK"
+                "\r\nContent-Type: text/html;"
+                "\r\nContent-Length: %d;"
+                "\r\n\r\n";
+            char* response = (char*)malloc(BUF_SIZE * 2 * sizeof(char));
+            memset(response, 0, BUF_SIZE * 2 * sizeof(char));
+
+            struct stat file_stat;
+            fstat(fd, &file_stat);
+            off_t file_size = file_stat.st_size;
+            size_t read_len = 0;
+            char* file_read = (char*)malloc(BUF_SIZE * sizeof(char));
+            memset(file_read, 0, BUF_SIZE * sizeof(char));
+
+            ssize_t bytes_read;
+            while ((bytes_read = read(fd, file_read + read_len, BUF_SIZE - read_len)) > 0) {
+                read_len += bytes_read;
+            }
+
+            sprintf(response, header, read_len);
+            strcat(response, file_read);
+
+            write(client_sock, response, strlen(response));
+            free(response);
+            free(file_read);
+        }
 
     }
     // Handle DELETE requests for deleting files.
-    
-    else if(strcasecmp(method, "DELETE") == 0){
+    else if (strcasecmp(method, "DELETE") == 0) {
         // Exercise 6: Implement DELETE here
         // Add your code here:
-        TODO();
+        char response[] =
+            "HTTP/1.1 202 Accepted"
+            "\r\nContent-Type: text/plain"
+            "\r\nContent-Length: 15"
+            "\r\n\r\ndelete received";
 
-    }else{
+        write(client_sock, response, strlen(response));
+    } else {
         char response[] = "HTTP/1.1 405 Method Not Allowed\r\nContent-Length: 0\r\n\r\n";
         write(client_sock, response, strlen(response));
     }
@@ -71,7 +111,7 @@ void serve_client(int client_sock, const char* web_root){
     close(client_sock);
 }
 
-int main(){
+int main() {
     char web_root[256];
     strcpy(web_root, getenv("HOME"));
     strcat(web_root, "/mywebsite");
@@ -81,13 +121,13 @@ int main(){
     socklen_t client_addr_len = sizeof(client_addr);
 
     server_sock = socket(AF_INET, SOCK_STREAM, 0);
-    if(server_sock < 0){
+    if (server_sock < 0) {
         perror("socket");
         exit(EXIT_FAILURE);
     }
 
     int reuseaddr = 1;
-    if(setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, &reuseaddr, sizeof(int)) == -1){
+    if (setsockopt(server_sock, SOL_SOCKET, SO_REUSEADDR, &reuseaddr, sizeof(int)) == -1) {
         perror("Setsockopt error");
         exit(EXIT_FAILURE);
     }
@@ -96,31 +136,31 @@ int main(){
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(SERVER_PORT);
     server_addr.sin_addr.s_addr = INADDR_ANY;
-    if(bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0){
+    if (bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
         perror("bind");
         exit(EXIT_FAILURE);
     }
 
-    if(listen(server_sock, 5) < 0){
+    if (listen(server_sock, 5) < 0) {
         perror("listen");
         exit(EXIT_FAILURE);
     }
 
     printf("Server listening on port %d...\n", SERVER_PORT);
 
-    while(1){
-        client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &client_addr_len);
+    while (1) {
+        client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &client_addr_len);
         if (client_sock < 0) {
             perror("accept");
             continue;
         }
 
-        
+
         serve_client(client_sock, web_root);
 
         // Challenge: multiple process
         // Add your code here:
-        TODO();
+        // TODO();
 
     }
 

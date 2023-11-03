36e43993d5e1d3f4bb932b1f5ab51bf43d4cf8bf
Aurutus <emslhy@hotmail.com>
2023-11-03 11:39:26 +0800

    lab: lab4 partA

--- lab4/partA/pi.c
+++ lab4/partA/pi.c
@@ -16,36 +16,41 @@ struct arg_t {
     float y;
 };
 
-void *start(void *arg) {
+void* start(void* arg) {
     // Exercise 2: Implement your code here
     // Add your code here:
-    TODO();
-
+    struct arg_t* _arg = (struct arg_t*)arg;
+    double d2 = _arg->x * _arg->x + _arg->y * _arg->y;
+    if (d2 <= 1.) {
+        pthread_exit((void*)1);
+        return NULL;
+    }
+    pthread_exit((void*)0);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
     long N1 = atoll(argv[1]);
     pthread_t tids[N1];
 
     // pi(concurrency)
-    for (long i = 0; i < N1; ++ i) {
-        struct arg_t *arg = malloc(sizeof(*arg));
+    for (long i = 0; i < N1; ++i) {
+        struct arg_t* arg = malloc(sizeof(*arg));
         int x = rand();
         int y = rand();
-        arg->x = 1.0*x / RAND_MAX;
-        arg->y = 1.0*y / RAND_MAX;
+        arg->x = 1.0 * x / RAND_MAX;
+        arg->y = 1.0 * y / RAND_MAX;
         pthread_create(&tids[i], 0, start, arg);
     }
 
-    void *res;
+    void* res;
     long M = 0;
-    for (long i = 0; i < N1; ++ i) {
+    for (long i = 0; i < N1; ++i) {
         pthread_join(tids[i], &res);
         M += (long)res;
     }
-    printf("pai = %f\n", 4.0*M/N1);
+    printf("pai = %f\n", 4.0 * M / N1);
 
-    assert(4.0*M/N1 > 3.1 && 4.0*M/N1 < 3.2);  
+    assert(4.0 * M / N1 > 3.1 && 4.0 * M / N1 < 3.2);
 
     pthread_exit(0);
 }
--- lab4/partA/square.c
+++ lab4/partA/square.c
@@ -11,23 +11,31 @@ do{\
 
 
 
-void *start(void *a) {
+void* start(void* a) {
     long n = (long)a;
-    pthread_exit((void *)(n * n));
+    pthread_exit((void*)(n * n));
 }
 
 int main() {
     long N = 10000;
     pthread_t tids[N + 1];
     long sum = 0;
+    void* res;
     // Exercise 1: Implement your code here
     // Add your code here:
-    TODO();
+    for (long i = 0; i < N + 1; i++) {
+        pthread_create(&tids[i], NULL, start, (void*)i);
+    }
 
-    printf("sum: %ld\n", sum);  
+    for (int i = 0; i < N + 1; i++) {
+        pthread_join(tids[i], &res);
+        sum += (long)res;
+    }
+
+    printf("sum: %ld\n", sum);
     // 添加断言
     long expected_sum = 0;
-    for (long i = 0; i <= N; ++ i) {
+    for (long i = 0; i <= N; ++i) {
         expected_sum += i * i;
     }
     assert(sum == expected_sum);  // 断言sum的值等于预期值

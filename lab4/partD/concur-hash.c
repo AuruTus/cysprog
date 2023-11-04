7ed2a420a9430caae6b239d5bd56c377c6be852b
Aurutus <emslhy@hotmail.com>
2023-11-04 18:04:20 +0800

    lab: finish lab4 partD

--- lab4/partD/concur-hash.c
+++ lab4/partD/concur-hash.c
@@ -15,47 +15,98 @@ do{\
 
 // hash
 /* this is the D. J. Bernstein hash function */
-static unsigned int djb_hash(const char *cp) {
+static unsigned int djb_hash(const char* cp) {
     unsigned hash = 5381;
-    while(*cp)
-        hash = 33 * hash ^ (unsigned char) *cp++;
+    while (*cp)
+        hash = 33 * hash ^ (unsigned char)*cp++;
     return hash;
 }
 
-void hash_init(hash_t *ht, int num_buckets){
+void hash_init(hash_t* ht, int num_buckets) {
     pthread_mutex_init(&ht->mu, 0);
-    ht->buckets = calloc(num_buckets, sizeof (*ht->buckets));
+    ht->buckets = calloc(num_buckets, sizeof(*ht->buckets));
+    for (int i = 0; i < num_buckets; i++) {
+        ht->buckets[i] = calloc(1, sizeof(node_t));
+        ht->buckets[i]->next = NULL;
+    }
     ht->num_buckets = num_buckets;
+    ht->size = 0;
 }
 
-void hash_insert(hash_t *ht, char *key, char *value) {
+void hash_insert(hash_t* ht, char* key, char* value) {
     // Exercise 2: complete the code below:
     // Add your code here:
-    TODO();
-
+    int hv = djb_hash(key); // the hash value
+    int n = hv % ht->num_buckets; // the bucket number
+    while (n < 0) {
+        n += ht->num_buckets;
+    }
+    pthread_mutex_lock(&ht->mu);
+    node_t* tmp = calloc(1, sizeof(node_t));
+    tmp->key = key;
+    tmp->value = value;
+    tmp->next = ht->buckets[n]->next;
+    ht->buckets[n]->next = tmp;
+    ht->size++;
+    pthread_mutex_unlock(&ht->mu);
 }
 
-char *hash_lookup(hash_t *ht, char *key) {
+char* hash_lookup(hash_t* ht, char* key) {
     // Exercise 2: complete the code below:
     // Add your code here:
-    TODO();
-
+    int hv = djb_hash(key); // the hash value
+    int n = hv % ht->num_buckets; // the bucket number
+    while (n < 0) {
+        n += ht->num_buckets;
+    }
+    char* val = NULL;
+    pthread_mutex_lock(&ht->mu);
+    node_t* curr = ht->buckets[n]->next;
+    while (curr) {
+        if (strcmp(curr->key, key) != 0) {
+            curr = curr->next;
+            continue;
+        }
+        val = curr->value;
+        break;
+    }
+    pthread_mutex_unlock(&ht->mu);
+    return val;
 }
 
 // return 0 for success; 1 for non-exiting "key"
-int hash_delete(hash_t *ht, char *key){
+int hash_delete(hash_t* ht, char* key) {
     // Exercise 2: complete the code below:
     // Add your code here:
-    TODO();
-
+    int hv = djb_hash(key); // the hash value
+    int n = hv % ht->num_buckets; // the bucket number
+    while (n < 0) {
+        n += ht->num_buckets;
+    }
+    pthread_mutex_lock(&ht->mu);
+    node_t* front = ht->buckets[n];
+    node_t* curr = ht->buckets[n]->next;
+    while (curr) {
+        if (strcmp(curr->key, key) != 0) {
+            front = curr;
+            curr = curr->next;
+            continue;
+        }
+        front->next = curr->next;
+        free(curr);
+        ht->size--;
+        break;
+    }
+    pthread_mutex_unlock(&ht->mu);
     return 1;
 }
 
-int hash_size(hash_t *ht){
+int hash_size(hash_t* ht) {
     int size = 0;
     // Exercise 2: complete the code below:
     // Add your code here:
-    TODO();
-
+    pthread_mutex_lock(&ht->mu);
+    size = ht->size;
+    pthread_mutex_unlock(&ht->mu);
     return size;
 }
--- lab4/partD/concur-stack.c
+++ lab4/partD/concur-stack.c
@@ -11,32 +11,47 @@ do{\
 
 
 
-void concur_stack_init(concur_stack_t *stack){
+void concur_stack_init(concur_stack_t* stack) {
     pthread_mutex_init(&stack->mu, NULL);
+    stack->top = (node_t*)malloc(sizeof(node_t));
+    stack->top->next = NULL;
+    stack->size = 0;
     return;
 }
 
-void concur_stack_push(concur_stack_t *stack, int value){
+void concur_stack_push(concur_stack_t* stack, int value) {
     // Exercise 1: complete other operations of the concurrent stack.
     // Add your code here:
-    TODO();
-
+    pthread_mutex_lock(&stack->mu);
+    node_t* new_top = (node_t*)malloc(sizeof(node_t));
+    new_top->value = value;
+    new_top->next = stack->top;
+    stack->top = new_top;
+    stack->size++;
+    pthread_mutex_unlock(&stack->mu);
 }
 
-int concur_stack_pop(concur_stack_t *stack){
+int concur_stack_pop(concur_stack_t* stack) {
     int value = -1;
     // Exercise 1: complete other operations of the concurrent stack.
     // Add your code here:
-    TODO();
-
+    pthread_mutex_lock(&stack->mu);
+    if (stack->top->next == NULL) {
+        return value;
+    }
+    value = stack->top->value;
+    stack->top = stack->top->next;
+    stack->size--;
+    pthread_mutex_unlock(&stack->mu);
     return value;
 }
 
-int concur_stack_size(concur_stack_t *stack){
+int concur_stack_size(concur_stack_t* stack) {
     int size = 0;
     // Exercise 1: complete other operations of the concurrent stack.
     // Add your code here:
-    TODO();
-
+    pthread_mutex_lock(&stack->mu);
+    size = stack->size;
+    pthread_mutex_unlock(&stack->mu);
     return size;
 }
